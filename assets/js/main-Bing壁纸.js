0e5b86c697db3705090b6ad4b1907346e3b08a97
kylehao <kylehao@gmail.com>
2024-12-01 16:21:53 +0000

    up

--- assets/js/main-Bing壁纸.js
+++ assets/js/main-Bing壁纸.js
@@ -27,7 +27,23 @@ var iUp = (function () {
 	};
 })();
 
-
+function getBingImages(imgUrls) {
+	/**
+	 * 获取Bing壁纸
+	 * 先使用 GitHub Action 每天获取 Bing 壁纸 URL 并更新 images.json 文件
+	 * 然后读取 images.json 文件中的数据
+	 */
+	var indexName = "bing-image-index";
+	var index = sessionStorage.getItem(indexName);
+	var panel = document.querySelector('#panel');
+	if (isNaN(index) || index == 1) index = 0;
+	else index++;
+	var imgUrl = imgUrls[index];
+	var url = "https://xjj-api.838383.xyz/pic/api.php";
+	panel.style.background = "url('" + url + "') center center no-repeat #666";
+	panel.style.backgroundSize = "cover";
+	sessionStorage.setItem(indexName, index);
+}
 
 function decryptEmail(encoded) {
 	var address = atob(encoded);
