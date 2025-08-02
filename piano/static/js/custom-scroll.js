d0dd9a1835e8131ea819a54dfb2f8a274b567b9b
kylehao <kylehao@gmail.com>
2025-08-02 03:22:25 +0000

    up

---
+++ piano/static/js/custom-scroll.js
@@ -0,0 +1,18 @@
+/*  Nvabar on clicking items */
+$(window).scroll(function(){	
+	"use strict";	
+	var scroll = $(window).scrollTop();
+	if( scroll > 60 ){		
+		$(".navbar").addClass("scroll-fixed-navbar");				
+	} else {
+		$(".navbar").removeClass("scroll-fixed-navbar");
+	}
+});
+
+/*  Smooth Scrolling on clicking nav items */
+$(".navbar-nav li a[href^='#']").on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $(this.hash).offset().top
+    }, 1000);
+});
